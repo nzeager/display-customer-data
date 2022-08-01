989e399a2e5bd6049b4cd4c65b28a70bb0606393
Nathan Zeager <nathanzeager@hotmail.com>
2022-08-01 19:43:35 -0400

    Displaying all information and updated CSS

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// data is being pulled from customers.js
+
 const container = document.querySelector('#container');
 
 function showCustomers(customerArray) {
@@ -9,14 +11,18 @@ function showCustomers(customerArray) {
         let imageDiv = document.createElement("div");
         imageDiv.classList.add("photo");
         let picture = document.createElement("img");
-        picture.src = customerInd.picture.thumbnail;
+        picture.src = customerInd.picture.large;
         imageDiv.appendChild(picture);
         customerDiv.appendChild(imageDiv);
 
         // name
         let nameDiv = document.createElement("div");
         nameDiv.classList.add("name");
-        nameDiv.innerText = `${customerInd.name.first} ${customerInd.name.last}`;
+        let firstName = customerInd.name.first;
+        let lastName = customerInd.name.last;
+        firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1); // capitalize first letter
+        lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+        nameDiv.innerText = `${firstName} ${lastName}`;
         customerDiv.appendChild(nameDiv);
         
         // email
@@ -34,20 +40,20 @@ function showCustomers(customerArray) {
         // address2
         let address2Div = document.createElement("div");
         address2Div.classList.add("address2");
-        let stateAbr = nameToAbbr(customerInd.location.state);
+        let stateAbr = nameToAbbr(customerInd.location.state); // using states.js
         address2Div.innerText = `${customerInd.location.city}, ${stateAbr} ${customerInd.location.postcode}`;
         customerDiv.appendChild(address2Div);
 
         // dob
         let dobDiv = document.createElement("div");
         dobDiv.classList.add("dob");
-        dobDiv.innerText = customerInd.dob.date;
+        dobDiv.innerText = "DOB: " + moment(customerInd.dob.date).format('ll'); // using moment.js
         customerDiv.appendChild(dobDiv);
 
         // startDate
         let startDateDiv = document.createElement("div");
         startDateDiv.classList.add("startDate");
-        startDateDiv.innerText = customerInd.registered.date;
+        startDateDiv.innerText = "Customer since: " + moment(customerInd.registered.date).format('ll'); // using moment.js
         customerDiv.appendChild(startDateDiv);
 
         // append customerDiv to container
